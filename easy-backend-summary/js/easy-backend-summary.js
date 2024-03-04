e267a65557acb766d3f9b7b4a077a9f66be8f724
Bjoernnn <staack@farn.de>
2024-03-04 16:25:25 +0100

    add ajax

--- easy-backend-summary/js/easy-backend-summary.js
+++ easy-backend-summary/js/easy-backend-summary.js
@@ -1,8 +1,18 @@
 jQuery(document).ready(function ($) {
-   
-    $("form").submit(function (e) {
+    $("#ebsum_set_id").submit(function (e) {
         e.preventDefault();
-        console.log("Submitted");
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+
+        var form = $(this);
         
+        $.ajax({
+            type: "POST",
+            url: "../wp-content/plugins/easy-backend-summary/db/db-handle.php",
+            data: form.serialize(), // serializes the form's elements.
+            success: function(data)
+            {
+              alert(data); // show response from the php script.
+            }
+        });
     });
 });
