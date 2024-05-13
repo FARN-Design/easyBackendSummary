db5015fa1b8f606fdd1446f3c166165054032de9
Marvin Taube <marvin.taube@pigeonport.de>
2024-05-13 13:37:35 +0200

    Replace sanitize_key with custom sanitize function in custom-db-handle.php
    The code change replaces the built-in sanitize_key function with a custom sanitization function called ebsum_sanitize in custom-db-handle.php. The new function can handle not only strings, but also arrays, for sanitization purposes. This custom sanitize function enhances the flexibility of our data handling by accommodating array values.

--- db/custom-db-handle.php
+++ db/custom-db-handle.php
@@ -5,6 +5,7 @@
  * @return void
  */
 function ebsum_db_handle(): void {
+
 	if ( ! isset( $_POST['is_submitted'])) {
 		return;
 	}
@@ -20,26 +21,26 @@ function ebsum_db_handle(): void {
 function ebsum_set_data_to_db(): void {
 
 	if (isset($_POST['post_types'][0]) && isset($_POST['user_roles'][0])) {
-		ebsum_set_settings( sanitize_key($_POST['post_types']), 'post_types', 'post_types' );
-		ebsum_set_settings( sanitize_key($_POST['user_roles']), 'user_roles', 'user_roles' );
+		ebsum_set_settings( ebsum_sanitize($_POST['post_types']), 'post_types', 'post_types' );
+		ebsum_set_settings( ebsum_sanitize($_POST['user_roles']), 'user_roles', 'user_roles' );
 	} elseif(isset( $_POST['user_roles'][0]) && !isset( $_POST['post_types'][0])){
-		ebsum_set_settings( sanitize_key($_POST['user_roles']), 'user_roles', 'user_roles' );
+		ebsum_set_settings( ebsum_sanitize($_POST['user_roles']), 'user_roles', 'user_roles' );
 		ebsum_set_settings( '', 'post_types', 'post_types' );
 	}elseif(!isset( $_POST['user_roles'][0]) && isset($_POST['post_types'][0])){
-		ebsum_set_settings( sanitize_key($_POST['post_types']), 'post_types', 'post_types' );
+		ebsum_set_settings( ebsum_sanitize($_POST['post_types']), 'post_types', 'post_types' );
 		ebsum_set_settings( '', 'user_roles', 'user_roles' );
 	}elseif(!isset( $_POST['user_roles'][0]) && !isset( $_POST['post_types'][0]) && isset( $_POST['is_post_and_user'][0])){
 		ebsum_set_settings( '', 'post_types', 'post_types' );
 		ebsum_set_settings( '', 'user_roles', 'user_roles' );
 	}else {
-		ebsum_set_settings( sanitize_key($_POST['quantity']), 'max_view', "" );
-		ebsum_set_settings( sanitize_key($_POST['period']), 'check_period', "" );
-		ebsum_set_settings( sanitize_key($_POST['loadlimit']), 'load_limit', "" );
+		ebsum_set_settings( ebsum_sanitize($_POST['quantity']), 'max_view', "" );
+		ebsum_set_settings( ebsum_sanitize($_POST['period']), 'check_period', "" );
+		ebsum_set_settings( ebsum_sanitize($_POST['loadlimit']), 'load_limit', "" );
 		if ( ! isset( $_POST['changes']) )  {
 			//set empty string to change_box in db if the checkbox of change is not set
 			ebsum_set_settings( '', 'change_box', "" );
 		} else {
-			ebsum_set_settings( sanitize_key($_POST['changes']), 'change_box', "" );
+			ebsum_set_settings( ebsum_sanitize($_POST['changes']), 'change_box', "" );
 		}
 	}
 }
@@ -53,7 +54,7 @@ function ebsum_set_data_to_db(): void {
  *
  * @return void
  */
-function ebsum_set_settings( array|string $post_array, string $key, string $value ): void {
+function ebsum_set_settings( array|string $post_array, string $key, string $value ) {
 
 	if ( is_array( $post_array ) ) {
 		$string = implode( "; ", $post_array );
@@ -65,11 +66,31 @@ function ebsum_set_settings( array|string $post_array, string $key, string $valu
 	$user_id = get_current_user_id();
 	global $wpdb;
 	$ebsum = $wpdb->prefix . 'easyBackendSummary';
-	$wpdb->update(
+
+	$x =  $wpdb->update(
 		$ebsum,
 		[ $key => $string ],
 		[ 'user_ID' => $user_id ]
 	);
+}
+
+/**
+ * Sanitizes an input value or an array of input values by removing any characters that are not allowed in a key
+ * or value of a database field.
+ *
+ * @param array|string $input The input value or array of input values to sanitize.
+ *
+ * @return string|array The sanitized input value or array of sanitized input values.
+ */
+function ebsum_sanitize(array|string $input):string|array{
+	if (is_array($input)){
+		$tmp = [];
+		foreach ($input as $key => $value){
+			$tmp[$key] = sanitize_key($value);
+		}
+		return $tmp;
+	}
+	return sanitize_key($input);
 }
 
 ?>
